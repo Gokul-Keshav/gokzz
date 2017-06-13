fd7adacec59c8fc75887a70525cb2250218d44fb
Gokul-Keshav <gokulkeshav7@gmail.com>
2017-06-13 14:49:59 +0530

    Create codekata.c

---
+++ codekata.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+  int a;
+  scanf("%d",&a);
+  if(a<0)
+  printf("The number is negative");
+  else if(a==0)
+  printf("The number is zero");
+  else
+  printf("The number is positive");
+  }
